84da10ab7bc13b5830b07bf3b7db00025ed9e807
Lalitha-Kalle <lalithaprogramming@gmail.com>
2025-07-11 14:44:38 +0530

    added auth routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './src/db/db.js'
+import authRouter from './src/routes/v1/auth.routes.js'
 dotenv.config()
 
 const app = express()
 const url = process.env.MONGO_URL
+
+app.use(express.json())
+
+app.use("/api/v1/auth", authRouter)
 
 app.get("/", (req, res) => {
   res.send("Welcome to Event Aggregator")
